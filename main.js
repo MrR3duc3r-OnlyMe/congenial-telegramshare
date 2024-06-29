0b576ddcf5eb2cfeed8a01647057bc40e2c9f1fd
wiegine@ech.dev <wiegine@ech.dev>
2024-06-30 00:48:29 +0800

    bol

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ app.get('/submit', async (req, res) => {
       });
     };
     await yello(cookies, url, amount, interval)
-    return res.json({
+    res.json({
       status: 200
     });
   } catch (err) {
